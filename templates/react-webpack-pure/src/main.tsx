5065e2e17a7b9544f87f907b937486a376757fb4
fkworld <fkworld@foxmail.com>
2023-06-17 17:28:22 +0800

    feat: add react-webpack-pure template project

---
+++ templates/react-webpack-pure/src/main.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+
+function App() {
+  return (
+    <div
+      css={{
+        fontSize: 42,
+        ":hover": { backgroundColor: "red" },
+      }}
+    >
+      hello react
+    </div>
+  );
+}
---
+++ templates/react-webpack-pure/webpack.config.mjs
@@ -0,0 +1,23 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+/** @type {import("webpack").Configuration & {devServer: import("webpack-dev-server").Configuration}} */
+export default {
+  mode: "development",
+  entry: "./src/main.tsx",
+  plugins: [new HtmlWebpackPlugin({ template: "./index.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        loader: "esbuild-loader",
+        options: {
+          tsconfig: "./tsconfig.json",
+        },
+      },
+    ],
+  },
+  devServer: {
+    port: 2077,
+    open: true,
+  },
+};
